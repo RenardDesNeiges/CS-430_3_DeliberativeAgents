695c631aef1ad9c0563d1fb771f08d4f88df03ea
salutctitou <titouan.renard@gmail.com>
2020-10-19 00:20:23 +0200

    Astar but fucked up

--- sourcecode/src/deliberative/Deliberative.java
+++ sourcecode/src/deliberative/Deliberative.java
@@ -1,13 +1,13 @@
 package deliberative;
 
-import java.util.Collection;
+import java.util.Collections;
 // Importing java utils
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
-import java.util.Objects;
 
-import ilog.concert.IloIntNaryTable.Iterator;
+import deliberative.State.Act;
+
+
 /* import table */
 import logist.simulation.Vehicle;
 import logist.agent.Agent;
@@ -23,24 +23,31 @@ import logist.topology.Topology.City;
  * An optimal planner for one vehicle.
  */
 
-class State{
+class State implements Comparable<State>{
+	enum Act {
+		MOVE, PICKUP, DELIVER, START
+	}
 	public logist.topology.Topology.City city;
 	public LinkedList<Task> ctask;
 	public LinkedList<Task> free_tasks;
 	public State parent;
 	public double cost;
-
+	public Act act;
+	public int depth;
 	/*
 	 * Basic constructor
 	 */
 	public State(logist.topology.Topology.City city, 
 			LinkedList<Task> ctask, LinkedList<Task> free_tasks, 
-			State parent, double cost){
+			State parent, double cost,Act act,int depth){
 		this.city = city;
 		this.ctask = ctask;
 		this.free_tasks = free_tasks;
 		this.parent = parent;
 		this.cost = cost;
+		this.act = act;
+		this.depth = depth;
+
 	}
 
 	/*
@@ -48,11 +55,13 @@ class State{
 	 */
 	public State(logist.topology.Topology.City city, 
 			LinkedList<Task> ctask,
-			TaskSet free_tasks, State parent, double cost) {
+			TaskSet free_tasks, State parent, double cost,Act act,int depth) {
 		this.city = city;
 		this.ctask = ctask;
 		this.parent = parent;
 		this.cost = cost;
+		this.act = act;
+		this.depth = depth;
 			
 		//converting TaskSet to LinkedList<Task>
 		LinkedList<Task> lFreeTasks = new LinkedList<Task>();
@@ -73,6 +82,11 @@ class State{
 		return sum;
 	}
 
+	public double heuristic(){
+		// C'est uNE hEUriSTIqUE (j'ai mis de constantes au bol, de façon heuristique tsais)
+		return 24000*this.free_tasks.size()+12000*this.ctask.size();
+	}
+
 	/*
 	 * Generates all possible states the agent might transition to from a given start
 	 * state (the successors on the graph)
@@ -82,8 +96,9 @@ class State{
 		
 		//generating movement actions
 		for (City stop : topology) {
-			if(this.city.hasNeighbor(stop)){
-				State nState = new State(stop, this.ctask, this.free_tasks, this, this.cost+this.city.distanceTo(stop)* capacity);
+			if (this.city.hasNeighbor(stop)) {
+				State nState = new State(stop, this.ctask, this.free_tasks, this,
+						this.cost + this.city.distanceTo(stop) * capacity,Act.MOVE,this.depth+1);
 				succ.push(nState);
 			}
 		}
@@ -99,7 +114,7 @@ class State{
 				nFreeTasks = (LinkedList) this.free_tasks.clone();
 				nFreeTasks.remove(task);
 				
-				State nState = new State(this.city, nCTask, nFreeTasks, this, this.cost);
+				State nState = new State(this.city, nCTask, nFreeTasks, this, this.cost,Act.PICKUP,this.depth+1);
 				succ.push(nState);
 			}
 		}
@@ -112,7 +127,7 @@ class State{
 				nCTask = (LinkedList) this.ctask.clone();
 				nCTask.remove(task);
 
-				State nState = new State(this.city, nCTask, this.free_tasks, this, this.cost);
+				State nState = new State(this.city, nCTask, this.free_tasks, this, this.cost,Act.DELIVER,this.depth+1);
 				succ.push(nState);
 			}
 		}
@@ -125,7 +140,7 @@ class State{
 	 * Returns true if state is a goal state
 	 */
 	public Boolean isGoal(){
-		if(this.free_tasks.size() == 0){
+		if(this.free_tasks.size() == 0 && this.ctask.size() == 0){
 			return true;
 		}
 		return false;
@@ -136,9 +151,13 @@ class State{
 	 */
 	@Override
 	public String toString() {
-		return this.city + ": cost:" + this.cost;
+		return this.city + "; cost: " + this.cost + "; act: "+ this.act + "; freeTasks : " + this.free_tasks.size()+"; depth: "+this.depth;
 	}
-
+	@Override
+	public int compareTo(State cmp){
+		Double cst = this.cost+this.heuristic();
+		return cst.compareTo(cmp.cost+cmp.heuristic());
+	}
 }
 
 public class Deliberative implements DeliberativeBehavior {
@@ -184,11 +203,11 @@ public class Deliberative implements DeliberativeBehavior {
 		switch (algorithm) {
 		case ASTAR:
 			// ...
-			plan = bfsPlanGenerator(vehicle, tasks);
+			plan = astarPlanGenerator(vehicle, tasks);
 			break;
 		case BFS:
 			// ...
-			plan = bfsPlanGenerator(vehicle, tasks);
+			plan = astarPlanGenerator(vehicle, tasks);
 			break;
 		default:
 			throw new AssertionError("Should not happen.");
@@ -197,36 +216,120 @@ public class Deliberative implements DeliberativeBehavior {
 	}
 
 	private State bfsAlgorithm(Vehicle vehicle, TaskSet tasks){
-		System.out.println(tasks);
-		Queue<State> Q = new LinkedList<State>();
-		Q.add(new State(vehicle.getCurrentCity(), new LinkedList<Task>(), tasks, null, 0.0)); //initializing the stack with the root node
+		LinkedList<State> Q = new LinkedList<State>();
+		Q.add(new State(vehicle.getCurrentCity(), new LinkedList<Task>(), tasks, null, 0.0,Act.START,0)); //initializing the stack with the root node
 		while (Q.size()>0){
-			State node = Q.remove(); //this is why we use a stack instead of a list
-			if(node.isGoal()){
-				return node;
-			}
+		Collections.sort(Q);
+		State node = Q.removeFirst();
+		if(node.isGoal()){
+			return node;
+		}
 		//Pushing the new states into the stack
-		//System.out.println(this.capacity);
 		Stack<State> newStates = node.succ(this.topology, this.capacity);
-		//System.out.println(newStates);
+
 		for (State st : newStates) {
 			Q.add(st);
 		}
+		System.out.println("depth : " + node.depth + "; free-tasks: " + node.free_tasks.size()+ "; ctasks : " + node.ctask.size() );
+
 	}
 		return null;
 	}
 
-	private LinkedList<State> backtrack(State endstate){
+	private State astarAlgorithm(Vehicle vehicle, TaskSet tasks) {
+		LinkedList<State> Q = new LinkedList<State>();
+		int dpth = 0;
+
+		Q.add(new State(vehicle.getCurrentCity(), new LinkedList<Task>(), tasks, null, 0.0, Act.START, 0)); // initializing the stack with the root node
+		while (Q.size() > 0) {
+			Collections.sort(Q);
+			State node = Q.removeFirst();
+			if (node.isGoal()) {
+				return node;
+			}
+			// Pushing the new states into the stack
+			Stack<State> newStates = node.succ(this.topology, this.capacity);
+
+			for (State st : newStates) {
+				Q.add(st);
+			}
+			double val = node.cost+node.heuristic();
+			/*System.out.println("depth : " + node.depth + "; free-tasks: " + node.free_tasks.size() + "; ctasks : "
+					+ node.ctask.size() + "; h : " + val );*/
+			if (node.depth > dpth) {
+				dpth = node.depth;
+			}
+
+		}
+		return null;
+	}
+
+	private Plan backtrack(State endstate){
 		LinkedList<State> path = new LinkedList<State>();
 		State current = endstate;
 		while(current != null){
 			path.add(current);
 			current = current.parent;
 		}
+		Plan plan = new Plan(path.get(path.size()-1).city);
 		for (int i = path.size(); i-- > 0;) {
-			System.out.println(path.get(i));
+			//System.out.println(i + ", s: " + path.get(i));
+			//System.out.println(i + ", f: " + path.get(i).free_tasks);
+			//System.out.println(i + ", c: " + path.get(i).ctask);
+			if(path.get(i).act == Act.MOVE){
+				plan.appendMove(path.get(i).city);
+			}
+			else if(path.get(i).act == Act.PICKUP){
+				//System.out.print("Picked up ");
+				for(Task tsk : path.get(i).ctask){
+					if(tsk.pickupCity == path.get(i).city){
+						plan.appendPickup(tsk);
+						//System.out.println(tsk);
+						break;
+					}
+				}
+			}
+			else if(path.get(i).act == Act.DELIVER){
+				//System.out.println(path.get(i+1).ctask);
+				//System.out.print("Delivered ");
+				for (Task tsk : path.get(i+1).ctask) {
+					//System.out.println(tsk+ " // " + path.get(i).city);
+					if (tsk.deliveryCity == path.get(i).city) {
+						//System.out.println(tsk);
+						plan.appendDelivery(tsk);
+						break;
+					}
+				}
+			}
+			//System.out.println("");
 		}
-		return path;
+		return plan;
+	}
+	
+	private Plan astarPlanGenerator(Vehicle vehicle, TaskSet tasks) {
+		Plan plan = backtrack(astarAlgorithm(vehicle, tasks));
+		//System.out.println(plan);
+		///// TESTING ENV
+		///////////////////////////////////////////////////////////////////////////////////////////
+
+		///////////////////////////////////////////////////////////////////////////////////////////
+		/*for (Task task : tasks) {
+			// move: current city => pickup location
+			for (City city : current.pathTo(task.pickupCity))
+				plan.appendMove(city);
+
+			plan.appendPickup(task);
+
+			// move: pickup location => delivery location
+			for (City city : task.path())
+				plan.appendMove(city);
+
+			plan.appendDelivery(task);
+
+			// set current city
+			current = task.deliveryCity;
+		}*/
+		return plan;
 	}
 
 	private Plan bfsPlanGenerator(Vehicle vehicle, TaskSet tasks) {
@@ -235,11 +338,8 @@ public class Deliberative implements DeliberativeBehavior {
 
 		///// TESTING ENV
 		///////////////////////////////////////////////////////////////////////////////////////////
-		System.out.println(backtrack(bfsAlgorithm(vehicle, tasks)));
+		backtrack(bfsAlgorithm(vehicle, tasks));
 
-		// System.out.println("Current city is : " + vehicle.getCurrentCity());
-		// State testState = new State(vehicle.getCurrentCity(), new LinkedList<Task>(), tasks, null, 0);
-		// System.out.println(testState.succ(this.topology, agent.vehicles().get(0).capacity()));
 		///////////////////////////////////////////////////////////////////////////////////////////
 		for (Task task : tasks) {
 			// move: current city => pickup location
